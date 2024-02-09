e4918410b8193d7a25f7634d1a7c6cf644751003
TomasChiari <tomaschiari01@gmail.com>
2024-02-09 17:51:13 -0300

    upload day two

---
+++ day-2/main.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementById("menu-icon");
+const lines = document.querySelectorAll(".no-animation");
+
+menuIcon.addEventListener("click", () => {
+	menuIcon.classList.toggle("active");
+	lines.forEach((line) => {
+		line.classList.remove("no-animation");
+	});
+});
